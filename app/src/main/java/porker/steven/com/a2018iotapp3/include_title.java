1989ff1f7103794d01b84e9df4a6f3e7c027a851
myhero <823961237@qq.com>
2018-03-26 21:42:27 +0800

    72页

---
+++ app/src/main/java/porker/steven/com/a2018iotapp3/include_title.java
@@ -0,0 +1,13 @@
+package porker.steven.com.a2018iotapp3;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class include_title extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_include_title);
+    }
+}
