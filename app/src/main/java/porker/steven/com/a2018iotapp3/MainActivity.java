c1a0f78b9274bb0ee7dd88d4beb05cc2d8c2d847
Steven-Lion <829361237@qq.com>
2018-03-30 10:23:01 +0800

    85衣

--- app/src/main/java/porker/steven/com/a2018iotapp3/MainActivity.java
+++ app/src/main/java/porker/steven/com/a2018iotapp3/MainActivity.java
@@ -2,12 +2,13 @@ package porker.steven.com.a2018iotapp3;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.curtain_acitivity);
     }
 }
